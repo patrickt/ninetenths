1782c735077b250096f9fdd99899626417cda65b
Patrick Thomson <patrickt@github.com>
2022-06-15 19:20:34 -0700

    tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use bracket_lib::prelude::*;
+
+struct State {
+}
+
+impl State {
+    fn new() -> State {
+        State {}
+    }
+
+}
+
+impl GameState for State {
+    fn tick(&mut self, _: &mut BTerm) {
+
+    }
+
+}
+
+fn run() -> BError {
+    let bterm = BTermBuilder::simple80x50().with_title("ninetenths").build()?;
+    main_loop(bterm, State::new())
+}
+
 fn main() {
     println!("Hello, world!");
+    run().unwrap();
 }
